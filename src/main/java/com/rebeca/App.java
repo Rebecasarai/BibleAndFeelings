0ecdf1ac53973f7ad19a57121387dc7a92b75fad
Rebeca Saraí <callmebeca@gmail.com>
2017-02-19 00:07:50 +0100

    Proyecto

---
+++ src/main/java/com/rebeca/App.java
@@ -0,0 +1,144 @@
+package com.rebeca;
+
+
+import java.io.*;
+import java.util.Random;
+
+/**
+ * Generate a unique number
+ *
+ */
+public class App{
+	private BufferedReader teclado=new BufferedReader(new InputStreamReader(System.in));
+	
+
+    public static void main( String[] args ) throws IOException
+    {
+    	  System.out.println();
+    	  System.out.println("Uno de los regalos que Dios nos ha dado es su palabra en forma de la Biblia. En ella podemos encontrar la verdad sobre Dios, consejos para el diario vivir y esperanza para mañana. Leerla es tan refrescante como tomarse un vaso de agua.");
+    	  System.out.println("");
+    	  App a = new App();
+    	  a.menu();
+
+    }
+
+    public void menu() throws IOException{
+		int opcion=0;
+		do {	
+			
+			 System.out.println("");
+			 System.out.println("==================================");
+			 System.out.println("¿Cómo te sientes? Lee versiculos por sentimiento o emociones que sientes.");
+			 System.out.println("1. Esperanza");
+			 System.out.println("2. Fe");
+			 System.out.println("3. Amor");
+			 System.out.println("4. Animo");
+			 System.out.println("5. Valentia");
+			 System.out.println("6. Superar la tristeza");
+			 System.out.println("7. Eliminar los celos");
+			 System.out.println("0. salir");
+			 System.out.println("");
+			 try{
+				 System.out.println("Seleccione una opcion");
+				 opcion=Integer.parseInt(teclado.readLine());
+			 }catch (NumberFormatException e) {
+				System.out.println("Debe introducir un numero de 0 a 6");
+				opcion=-1;
+			 }
+			 
+			 switch (opcion) {
+			case 1:
+				esperanza();
+				break;
+			case 2:
+				fe();
+				break;
+
+			case 3:
+				amor();
+				break;
+
+			case 4:
+				animo();
+				break;
+			case 5:
+				valentia();
+				break;
+			case 6:
+				superarTristeza();
+				break;
+			case 7:
+				eliminarCelos();
+				break;
+			case 0:
+				System.out.println("Dios te acompañe y guie. Ha salido del menú");
+				break;
+			default:
+				System.out.println("Debe introducir un numero entre 0 y 4");
+				break;
+			}
+		} while (opcion!=0);	
+		
+		
+	}
+
+	private void amor() {
+		Random rand = new Random();
+
+        System.out.println(Versiculos.AMOR[rand.nextInt(Versiculos.AMOR.length)]);
+    	
+	}
+
+	private void esperanza() {
+    	Random rand = new Random();
+
+        System.out.println(Versiculos.ESPERANZA[rand.nextInt(Versiculos.ESPERANZA.length)]);
+    	
+    
+    }
+
+	private void fe() {
+		Random rand = new Random();
+
+        System.out.println(Versiculos.FE[rand.nextInt(Versiculos.FE.length)]);
+    	
+		
+	}
+    
+    private void animo() {
+    	Random rand = new Random();
+
+        System.out.println(Versiculos.ANIMO[rand.nextInt(Versiculos.ANIMO.length)]);
+    	
+		
+	}
+
+	private void valentia() {
+		Random rand = new Random();
+
+        System.out.println(Versiculos.VALENTIA[rand.nextInt(Versiculos.VALENTIA.length)]);
+    	
+		
+	}
+
+	private void eliminarCelos() {
+		Random rand = new Random();
+
+        System.out.println(Versiculos.DELETECELOS[rand.nextInt(Versiculos.DELETECELOS.length)]);
+    	
+		
+	}
+
+	private void superarTristeza() {
+		Random rand = new Random();
+
+        System.out.println(Versiculos.DELETETRISTEZA[rand.nextInt(Versiculos.DELETETRISTEZA.length)]);
+    	
+		
+	}
+
+
+    
+    
+		
+}
---
+++ src/main/java/com/rebeca/Versiculos.java
@@ -0,0 +1,65 @@
+package com.rebeca;
+
+public class Versiculos {
+	public static final String  ESPERANZA[] = {
+            "Pero los que confían en el Señor\nrenovarán sus fuerzas;\nvolarán como las águilas:\ncorrerán y no se fatigarán,\ncaminarán y no se cansarán.\n― Isaías 40:31",
+            "El Señor te protegerá;\nde todo mal protegerá tu vida.\nEl Señor te cuidará en el hogar y en el camino,\ndesde ahora y para siempre.― Salmos 121:7-8",
+            "Que el Dios de la esperanza los llene de toda alegría y paz a ustedes que creen en él, para que rebosen de esperanza por el poder del Espíritu Santo.\n- Romanos 15:13 ",
+            "Ahora bien, la fe es la garantía de lo que se espera, la certeza de lo que no se ve.\n- Hebreos 11:1",
+            "Vengan a mí todos ustedes que están cansados y agobiados, y yo les daré descanso―\n Mateo 11:28",
+            "Mantengamos firme la esperanza que profesamos, porque fiel es el que hizo la promesa.\n― Hebreos 10:23",
+            "¡Alabado sea Dios, Padre de nuestro Señor Jesucristo! Por su gran misericordia, nos ha hecho nacer de nuevo mediante la resurrección de Jesucristo, para que tengamos una esperanza viva.\n― 1 Pedro 1:3",
+            "A éstos Dios se propuso dar a conocer cuál es la gloriosa riqueza de este misterio entre las naciones, que es Cristo en ustedes, la esperanza de gloria.\n ― Colosenses 1:27",                     
+            };   
+	public static final String  FE[] = {
+            "Por eso les digo: Crean que ya han recibido todo lo que estén pidiendo en oración, y lo obtendrán.\n-Marcos 11:24",
+            "Ahora bien, la fe es la garantía de lo que se espera, la certeza de lo que no se ve.\n-Hebreos 11:1",
+            "Pero que pida con fe, sin dudar, porque quien duda es como las olas del mar, agitadas y llevadas de un lado a otro por el viento.\n-Santiago 1:6",
+            "—¿Cómo que si puedo? Para el que cree, todo es posible.\n-Marcos 9:23",
+            "Les aseguro que si alguno le dice a este monte: “Quítate de ahí y tírate al mar”, creyendo, sin abrigar la menor duda de que lo que dice sucederá, lo obtendrá.\n-Marcos 11:23",
+                       
+            };   
+	public static final String  AMOR[] = {
+            "Por encima de todo, vístanse de amor, que es el vínculo perfecto. \n-Colosenses 3:14",
+            "Y nosotros hemos llegado a saber y creer que Dios nos ama. Dios es amor. El que permanece en amor, permanece en Dios, y Dios en él. \n-1 Juan 4:16",
+            "Y éste es mi mandamiento: que se amen los unos a los otros, como yo los he amado. \n -Juan 15:12",
+            "Si alguien afirma: «Yo amo a Dios», pero odia a su hermano, es un mentiroso; pues el que no ama a su hermano, a quien ha visto, no puede amar a Dios, a quien no ha visto. -1 Juan 4:20",
+            "Hagan todo con amor. \n -1 Corintios 16:14",
+            "El amor es paciente, es bondadoso. El amor no es envidioso ni jactancioso ni orgulloso. No se comporta con rudeza, no es egoísta, no se enoja fácilmente, no guarda rencor. \n-1 Corintios 13:4-5",
+            "Ahora, pues, permanecen estas tres virtudes: la fe, la esperanza y el amor. Pero la más excelente de ellas es el amor. \n-1 Corintios 13:13",
+            "El odio es motivo de disensiones,\npero el amor cubre todas las faltas.\n-Proverbios 10:12",
+            "El segundo es: “Ama a tu prójimo como a ti mismo.” No hay otro mandamiento más importante que éstos.\n-Marcos 12:31",
+            "Si hablo en lenguas humanas y angelicales, pero no tengo amor, no soy más que un metal que resuena o un platillo que hace ruido. \n-1 Corintios 13:1",
+            "El que no ama no conoce a Dios, porque Dios es amor. \n-1 Juan 4:8",                          
+            };   
+	public static final String  ANIMO[] = {
+            "No tengas miedo, que yo estoy contigo; no te desanimes, que yo soy tu Dios. Yo soy quien te da fuerzas, y siempre te ayudaré; siempre te sostendré con mi justiciera mano derecha. \n-Isaías 41:10 ",
+            "No se turbe su corazón. Ustedes creen en Dios; crean también en mí.\n-Juan 14:1",
+            "¡Todo lo puedo en Cristo que me fortalece!\n -Filipenses 4:13",
+            "El Señor es bueno;\nes un refugio en el día de la angustia.\nEl Señor conoce a los que en él confían,\n-Nahúm 1:7",
+            "Ya te lo he ordenado: ¡Sé fuerte y valiente! ¡No tengas miedo ni te desanimes! Porque el Señor tu Dios te acompañará donde quiera que vayas.\n-Josué 1:9",
+                   
+            };   
+	public static final String  VALENTIA[] = {
+            "No tengas miedo, que yo estoy contigo; no te desanimes, que yo soy tu Dios. Yo soy quien te da fuerzas, y siempre te ayudaré; siempre te sostendré con mi justiciera mano derecha. \n-Isaías 41:10 ",
+            "No se turbe su corazón. Ustedes creen en Dios; crean también en mí.\n-Juan 14:1",
+            "¡Todo lo puedo en Cristo que me fortalece!\n -Filipenses 4:13",
+            "El Señor es bueno;\nes un refugio en el día de la angustia.\nEl Señor conoce a los que en él confían,\n-Nahúm 1:7",
+            "Ya te lo he ordenado: ¡Sé fuerte y valiente! ¡No tengas miedo ni te desanimes! Porque el Señor tu Dios te acompañará donde quiera que vayas.\n-Josué 1:9",
+                   
+            };   
+	public static final String  DELETECELOS[] = {
+            "No envidie tu corazón a los pecadores, antes {vive} siempre en el temor del Señor;\n-Proverbios 23:17",
+            "Porque nosotros también en otro tiempo éramos necios, desobedientes, extraviados, esclavos de deleites y placeres diversos, viviendo en malicia y envidia, aborrecibles {y} odiándonos unos a otros.\n-Tito 3:3",
+            "El amor es paciente, es bondadoso; el amor no tiene envidia; el amor no es jactancioso, no es arrogante;\n-1 Corintios 13:4",
+            "Por tanto, desechando toda malicia y todo engaño, e hipocresías, envidias y toda difamación,\n-1 Pedro 2:1",
+                 
+            };  
+	public static final String  DELETETRISTEZA[] = {
+            "Echa sobre Jehová tu carga, y él te sustentará;\nNo dejará para siempre caído al justo.\n-Salmos 55:22",
+            "Y Jehová va delante de ti; él estará contigo, no te dejará, ni te desamparará; no temas ni te intimides\n-Deuteronomio 31:8",
+            "Mejor es la tristeza que la risa, porque cuando el rostro está triste el corazón puede estar contento.\n-Eclesiastés 7:3",
+            "Cercano está el SEÑOR a los quebrantados de corazón, y salva a los abatidos de espíritu.\n-Salmos 34:18",
+              
+            }; 
+}
