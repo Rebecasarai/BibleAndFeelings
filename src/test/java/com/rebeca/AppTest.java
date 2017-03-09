07f293c7e377ee67257e13b18c8673bf8cfd46ad
rebecasarai <callmebeca@gmail.com>
2017-03-09 20:39:58 +0100

    m

--- src/test/java/com/rebeca/AppTest.java
+++ src/test/java/com/rebeca/AppTest.java
@@ -14,14 +14,15 @@ public class AppTest
     extends TestCase
 {
 	@Test
-	  public void testPrintHola() {
+	  public void testCrearVersiculos() {
 		Versiculos v = new Versiculos("Isaias", "42:6", "Yo Jehová te he llamado en justicia, y te sostendré por la mano; te guardaré y te pondré por pacto al pueblo, por luz de las naciones.");
 		  assertEquals("Isaias", v.getLibro());
 		  assertEquals("42:6", v.getNumVersiculo());
 		  assertEquals("Yo Jehová te he llamado en justicia, y te sostendré por la mano; te guardaré y te pondré por pacto al pueblo, por luz de las naciones.", v.getVersiculo());
 		  
-    
  }
+	
+	
     
     
 	
